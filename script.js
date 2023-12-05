f7eee8053a0ccc085ced0fc60adf6f5ca8668025
saikumar74 <143096449+saikumar74@users.noreply.github.com>
2023-12-05 16:51:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+let [ seconds, minutes, hours] = [ 0, 0, 0];
+let timeRef = document.querySelector(".timer-display");
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click", () => {
+    if(int !== null) {
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
+});
+
+document.getElementById("pause-timer").addEventListener("click", () => {
+    clearInterval(int);
+});
+
+document.getElementById("reset-timer").addEventListener("click", () => {
+    clearInterval(int);
+    [ seconds, minutes, hours] = [0, 0, 0];
+    timeRef.innerHTML = "00 : 00 : 00  ";
+}); 
+
+function displayTimer() {
+    seconds +=10;
+        if(seconds == 60) {
+            seconds = 0;
+            minutes++;
+            if(minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    // let s = 
+        seconds < 10
+        ? "00" + seconds
+        : seconds < 100
+        ? "0" + seconds
+        : seconds;
+
+    timeRef.innerHTML = `${h} : ${m} : ${s} `;
+
+}
